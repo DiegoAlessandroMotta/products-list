2748fc7d3eeffb350696ed578d31a5eabaf185da
DiegoAlessandroMotta <diegomottaq@gmail.com>
2024-05-01 09:58:54 -0500

    Add products list

---
+++ products.js
@@ -0,0 +1,52 @@
+export const products = [
+  {
+    id: 1,
+    title: 'Gamer Asus Intel Core i5 RTX3050 8GB 512GB SSD TUF Gaming F15 12°Gen 15.6"',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/882958599_1?wid=500&hei=500&qlt=7'
+  },
+  {
+    id: 2,
+    title: 'Laptop Asus Intel Core I7 De 10 Nucleos 16Gb Ram 1 Tb Ssd Vivobook 15 13° Gen 15.6"',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/883163878_1?wid=500&hei=500&qlt=70'
+  },
+  {
+    id: 3,
+    title: 'Monitor ASUS ROG Strix XG27AQV 27", WQHD, Fast IPS, 170 Hz, 1 ms GTG',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/gsc_124452483_3985973_1?wid=500&hei=500&qlt=70'
+  },
+  {
+    id: 4,
+    title: 'Monitor Samsung LS27ag320nlxpe 27 FHD IPS 165hz 1MS FRESYNG PREMIUN',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/114059585_1?wid=500&hei=500&qlt=70'
+  },
+  {
+    id: 5,
+    title: 'Monitor samsung s19a330nhl 18.5 hd led ips vga hdmi',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/gsc_114131178_857729_1?wid=500&hei=500&qlt=70'
+  },
+  {
+    id: 6,
+    title: 'Monitor SAMSUNG LS24R350FZNXZA 24 IPS FULL HD 75HZ 5MS HDMI VGA',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/gsc_121406678_3022642_1?wid=500&hei=500&qlt=70'
+  },
+  {
+    id: 7,
+    title: 'Galaxy S24+ 5g 512gb Marble Gray',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/20178946_02?wid=500&hei=500&qlt=70'
+  },
+  {
+    id: 8,
+    title: 'Smartphone Galaxy ZFLIP5 512GB 8GB Verde Menta',
+    imgUrl: 'https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/tottusPE/43153736_1/w=500,h=500'
+  },
+  {
+    id: 9,
+    title: 'Apple Macbook Air 13" Chip M1 8gb Ram 256 Gb Ssd - Space Gray',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/17924885_1?wid=500&hei=500&qlt=70'
+  },
+  {
+    id: 10,
+    title: 'Computadora PC Gamer Core i7 Ram 16gb 1TB+SSD 240GB Video GT 740 4gb Mon 24 FHD',
+    imgUrl: 'https://falabella.scene7.com/is/image/FalabellaPE/gsc_120235675_2606937_1?wid=500&hei=500&qlt=70'
+  }
+]
